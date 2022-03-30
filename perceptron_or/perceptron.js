6a7c834e4bb97a859608b49c61d699c2d11ee288
Jan Samak <jan_karim.samak@smail.th-koeln.de>
2022-03-30 18:12:06 +0200

    feat: refactoring and visualisation of `and` and `or`

--- perceptron_or/perceptron.js
+++ perceptron_or/perceptron.js
@@ -1,41 +1,50 @@
-//example code for a btework computing the or function
+let inputNodes = [[1, 0, 0], [1, 0, 1], [1, 1, 0], [1, 1, 1]];
+let outputNodes = [[],[]];
+let weights = [[-1, 1, 1], [0, 1, 1]];
 
-//array with all possible combination for the three expressions
-let x = [[1, 0, 0],[1, 0, 1], [1, 1, 0],[1, 1, 1]];
-let y =[];
-//weight combination for realizing the or function
-let w=[0,1,1];
 function setup() {
     createCanvas(400, 400);
     noLoop();
+    textSize(32);
+    colorMode(HSB, 360, 1, 1, 1);
+    strokeWeight(0);
 }
 
 function draw() {
     background(255);
-    colorMode(HSB, 360, 1, 1, 1);
-    strokeWeight(0);
-    //compute the perceptron response for all 4 combinations of the truth table
-    for (i = 0; i < 4; i++) {
-	fill(0, 1, 0.1 + x[i][1]);
-	ellipse(100, 70 + 75 * i, 50, 50);
-	fill(0, 1, 0.1 + x[i][2]);
-	ellipse(175, 70 + 75 * i, 50, 50);
-	//here the response for the neuron is computed
-	y[i] = threshold(x[i][0] * w[0] + x[i][1] * w[1] + x[i][2] * w[2]);
-	fill(0,1,0.1+ y[i]);
-	ellipse(275,70+75*i,50,50);
+    for (let i = 0; i < 4; i++) {
+        drawNode(inputNodes[i][1], 0, i);
+        fill(0, 1, 0.1 + inputNodes[i][2]);
+        ellipse(175, 70 + 75 * i, 50, 50);
+        drawNode(inputNodes[i][2], 1, i);
+        //here the response for the neuron is computed
+        outputNodes[0][i] = threshold(inputNodes[i][0] * weights[0][0] + inputNodes[i][1] * weights[0][1] + inputNodes[i][2] * weights[0][2]);
+        drawNode(outputNodes[0][i], 2, i);
+        outputNodes[1][i] = threshold(inputNodes[i][0] * weights[1][0] + inputNodes[i][1] * weights[1][1] + inputNodes[i][2] * weights[1][2]);
+        drawNode(outputNodes[1][i], 3, i);
+        setText()
     }
-    textSize(32);
-    strokeWeight(0);
-    //noFill();
+
+}
+
+function drawNode(fillValue, column, row) {
+    let startPoint = [100, 70]
+    let padding = 75
+    let radius = 50
+    fill(0, 1, 0.1 + fillValue);
+    ellipse(startPoint[0] + padding * column, startPoint[1] + padding * row, radius, radius);
+}
+
+function setText() {
     text("x 1", 80, 25);
-    text("x 2", 160, 25);
-    text("y ", 260, 25);
+    text("x 2", 155, 25);
+    text("and", 225, 25);
+    text("or", 310, 25);
 }
 
 function threshold(input) {
     if (input > 0)
-	return 1;
+        return 1;
     else
-	return 0;
+        return 0;
 }
